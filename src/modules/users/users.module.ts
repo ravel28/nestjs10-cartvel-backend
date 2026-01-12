2fef0ae3b573e1ff0c9d629275b35ddf39b8e43d
ravel28 <ravelinno9@gmail.com>
2026-01-12 15:18:34 +0700

    [update] remove mitra service

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { PrismaService } from 'src/cores/services/prisma.service';
-import { MitraService } from './services/mitra.service';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService, MitraService, PrismaService],
+  providers: [UsersService, PrismaService],
 })
 export class UsersModule {}
